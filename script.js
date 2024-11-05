245cde45d2481bc233e9482add51d27b60d6cc02
Fasterino <166503398+Rfn18@users.noreply.github.com>
2024-11-05 20:03:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -16,10 +16,33 @@ const namaKhodam = [
   "Kipas Angin",
   "Botol",
   "Minion",
+  "Macan pemarah",
+  "Buaya sunda",
+  "Beruang sunda",
+  "Harimau birahi",
+  "Tutup odol",
+  "Tutup panci",
+  "Kaleng kejepit",
+  "Kanebo kering",
+  "Kapal karam",
+  "Gergaji mesin",
+  "Nyi blorong",
+  "Jin rawa rontek",
+  "Kucing Israel",
+  "Capung gila",
+  "Tumis kangkung",
+  "Jam Rusak",
 ];
 
 function checkKhodam() {
-  const random = Math.floor(Math.random() * namaKhodam.length);
-  khodam.innerHTML = namaKhodam[random];
-  nama.innerHTML = input.value;
+  setTimeout(() => {
+    input.value = "";
+  }, 100);
+  if (input.value === "") {
+    return;
+  } else {
+    const random = Math.floor(Math.random() * namaKhodam.length);
+    khodam.innerHTML = namaKhodam[random];
+    nama.innerHTML = input.value;
+  }
 }
